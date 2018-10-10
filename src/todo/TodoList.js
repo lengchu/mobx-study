8fb83fe3e7b303efeff137cf03b2d668f4a36a33
lenchu <553308706@qq.com>
2018-10-10 20:05:49 +0800

    refact todo with mdb

--- src/todo/TodoList.js
+++ src/todo/TodoList.js
@@ -1,9 +1,9 @@
-import { observable, computed } from 'mobx'
+import { observable, computed, action } from 'mobx'
 import TodoView, { Todo } from './Todo'
 import { observer } from 'mobx-react'
 import React from 'react'
 import scssStyles from './style.module.scss'
-import { Button } from 'mdbreact'
+import { Input, Button, ToastContainer, toast } from 'mdbreact'
 
 export class TodoList {
     @observable todos = [
@@ -14,10 +14,22 @@ export class TodoList {
     @computed get unfinishedTodoCount() {
         return this.todos.filter(todo => !todo.finished).length
     }
+
+    @action
+    addTodo(todo) {
+        this.todos.push(todo)
+    }
 }
 
 @observer
 class TodoListView extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            newTodo: ''
+        }
+    }
 
     render() {
       return (
@@ -28,9 +40,27 @@ class TodoListView extends React.Component {
               ))}
           </ul>
           Tasks left: {this.props.todoList.unfinishedTodoCount}
-          <Button>Test</Button>
+          <Input value={this.state.newTodo} label="Add a Todo" getValue={(value) => this.setState({newTodo: value})} />
+          <Button onClick={this.addTodo}>Save</Button>
+          <ToastContainer
+            hideProgressBar={true}
+            newestOnTop={true}
+            autoClose={3000}
+          />
         </div>
       )
+    }
+
+    addTodo = () => {
+        if(this.state.newTodo === '')
+            toast.error('todo can not be empty')
+        else if(this.props.todoList.todos.map(todo => todo.title).includes(this.state.newTodo))
+            toast.error('todo exists')
+        else {
+            this.props.todoList.addTodo(new Todo(this.state.newTodo, false))
+            this.setState({newTodo: ''})
+            toast.success('add success')
+        }
     }
 }
 
